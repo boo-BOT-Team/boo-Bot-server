e5fc29546beea2151459c9357a640cebe547ec73
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-08-29 10:51:22 -0700

    added GET route for themes

---
+++ __tests__/themes.test.js
@@ -0,0 +1,25 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('user routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('#GET /api/v1/themes should get all themes', async () => {
+    const res = await request(app).get('/api/v1/themes');
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual(expect.arrayContaining([
+      {
+        id: expect.any(String),
+        name: expect.any(String)
+      }
+    ]));
+  });
+
+});
