a1a65c9e4e0a63812c55805f8f19d06798bbb2b9
KASHI TAMANG <tamang.kashi@gmail.com>
2022-08-29 11:08:42 -0700

    adding #DELETE route for users, pasing test

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -1,6 +1,5 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const { request } = require('express');
 const request = require('supertest');
 const app = require('../lib/app');
 
@@ -35,5 +34,17 @@ describe('user routes', () => {
       .send({ email: 'booemail@ghost.com', password: 'iamghost' });
     expect(res.status).toBe(200);
     expect(res.body.message).toEqual('Sign in successful');
+  });
+
+  it('#DELETE signs out a user', async () => {
+    await request(app).post('/api/v1/users').send(booUser);
+    await request(app).post('/api/v1/users/sessions').send(booUser);
+    const res = await request(app).delete('/api/v1/users/sessions');
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      success: true,
+      message: 'Signed out successfully'
+    });
   });
 });
