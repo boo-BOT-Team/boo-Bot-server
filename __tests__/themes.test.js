f8956b5986649e306ec917cfbbff3dc980a7570d
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-08-29 11:25:26 -0700

    added POST route for themes

--- __tests__/themes.test.js
+++ __tests__/themes.test.js
@@ -11,6 +11,22 @@ describe('user routes', () => {
     pool.end();
   });
 
+  const mockUser = {
+    email: 'testUser@user.com',
+    password: '123456'
+  };
+
+  const newTheme = {
+    name: 'Mummy'
+  };
+
+  const registerAndLogin = async (agent, user) => {
+    const res = await agent.post('/api/v1/users').send(user);
+    expect(res.status).toBe(200);
+    return res.body;
+  };
+
+
   it('#GET /api/v1/themes should get all themes', async () => {
     const res = await request(app).get('/api/v1/themes');
     expect(res.status).toBe(200);
@@ -20,6 +36,19 @@ describe('user routes', () => {
         name: expect.any(String)
       }
     ]));
+  });
+
+  it('#POST /api/v1/themes should post a new theme', async() => {
+    const agent = request.agent(app);
+    await registerAndLogin(agent, mockUser);
+    const res = await agent.post('/api/v1/themes').send(newTheme);
+    
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      name: 'Mummy'
+    });
+
   });
 
 });
