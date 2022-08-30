6c74cf57626d36df618d7001a2266e0607a01981
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-08-30 10:56:58 -0700

    added GET random  route

--- __tests__/facts.test.js
+++ __tests__/facts.test.js
@@ -21,15 +21,15 @@ describe('user routes', () => {
     });
   });
 
-//   it('#GET /api/v1/links/random should return a random link', async () => {
-//     const res = await request(app).get('/api/v1/links/random');
-//     expect(res.status).toBe(200);
-//     expect(res.body).toEqual(
-//       expect.objectContaining({
-//         id: expect.any(String),
-//         url: expect.any(String),
-//         themeId: expect.any(String),
-//       })
-//     );
-//   });
+  it('#GET /api/v1/facts/random should return a random fact', async () => {
+    const res = await request(app).get('/api/v1/facts/random');
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        content: expect.any(String),
+        themeId: expect.any(String),
+      })
+    );
+  });
 });
